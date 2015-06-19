7570595a3b823184b60bb6361774ce0a08d2ecfd
Srinivas Maroju <marojupavan@gmail.com>
2015-06-19 10:26:49 -0400

    First commit

---
+++ Moneta/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Moneta
+//
+//  Created by Srinivas Maroju on 6/18/15.
+//  Copyright (c) 2015 Srinivas Maroju. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
